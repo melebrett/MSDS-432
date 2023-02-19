295aacfd398727f3cbb6011a73056e3ed4646ed0
melebrett <brett.mele@sumersports.com>
2023-02-19 17:40:00 -0600

    community boundaries

--- pipelines/community-boundaries/community_boundaries_pipeline.go
+++ pipelines/community-boundaries/community_boundaries_pipeline.go
@@ -1,3 +1,215 @@
 package main
 
-func main()
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+// API endpoint URL
+const url = "https://data.cityofchicago.org/resource/igwz-8jzy.json"
+
+// Define struct for individual records
+type CommunityBoundary struct {
+	Geom      string `json:"the_geom"`
+	AreaNum   string `json:"area_numbe"`
+	Community string `json:"community"`
+	ShapeArea string `json:"shape_area"`
+	ShapeLen  string `json:"shape_len"`
+}
+
+var Boundaries []CommunityBoundary
+
+func GetAPIrequest(url string) []CommunityBoundary {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("Error: API get request failed. %v", err)
+	}
+	defer resp.Body.Close()
+
+	// TESTING PRINT
+	fmt.Println("API request completed")
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error: Failed to read API response: %v", err)
+	}
+
+	// TESTING PRINT
+	fmt.Println("Response read successfully")
+
+	if err := json.Unmarshal(body, &Boundaries); err != nil {
+		fmt.Printf("Cannot unmarshal JSON: %v", err)
+	}
+
+	return Boundaries
+}
+
+// function for saving JSON file for testing - eliminate excessive API calls
+func SaveTripsJSON(filename string) {
+	content, err := json.Marshal(Boundaries)
+	if err != nil {
+		log.Fatalf("Error while marshaling struct: %v", err)
+	}
+	err = os.WriteFile(filename, content, 0777)
+	if err != nil {
+		log.Fatalf("Error while writing to json file: %v", err)
+	}
+}
+
+// function for loading the saved JSON file for testing - eliminate excessive API calls
+func LoadTripsJSON(filename string) {
+	input, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("Error while reading json file %v", err)
+	}
+	err = json.Unmarshal(input, &Boundaries)
+	if err != nil {
+		log.Fatalf("Error while unmarshaling json to struct: %v", err)
+	}
+}
+
+func DbConnect() (*sql.DB, error) {
+	//Retreiving DB connection credential environment variables
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("Could not load .env file")
+	}
+
+	HOST := os.Getenv("HOST")
+	PORT := os.Getenv("PORT")
+	USER := os.Getenv("USER")
+	PASSWORD := os.Getenv("PASSWORD")
+	DBNAME := os.Getenv("DBNAME")
+
+	DB_DSN := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", HOST, PORT, USER, PASSWORD, DBNAME)
+
+	db, err := sql.Open("postgres", DB_DSN)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// err = db.Ping()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	fmt.Println("Successfully connected to DB")
+
+	return db, nil
+}
+
+func refresh_db_table() {
+	db, err := DbConnect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	dropTableStatement := "DROP TABLE IF EXISTS community_boundaries;"
+
+	_, err = db.Exec(dropTableStatement)
+	if err != nil {
+		panic(err)
+	}
+
+	createTableStatement := `CREATE TABLE community_boundaries (
+		AreaNum		TEXT PRIMARY KEY,
+		Community   TEXT,
+		Geom        TEXT,
+		ShapeArea	TEXT,
+		ShapeLen	TEXT
+		);`
+
+	_, err = db.Exec(createTableStatement)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func load_to_db(Boundaries []CommunityBoundary) {
+	db, err := DbConnect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	insertStatement := `INSERT INTO community_boundaries (AreaNum, Community, Geom, ShapeArea, ShapeLen) 
+							values ($1, $2, $3, $4, $5)
+							ON CONFLICT (AreaNum) 
+							DO NOTHING;`
+
+	for _, v := range Boundaries {
+		_, err = db.Exec(insertStatement,
+			v.AreaNum,
+			v.Community,
+			v.Geom,
+			v.ShapeArea,
+			v.ShapeLen,
+		)
+		if err != nil {
+			fmt.Printf("Error inserting record, Community = %v", v.Community)
+		}
+	}
+}
+
+func test_successful_insert() {
+	db, err := DbConnect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	testStatement1 := "SELECT Community FROM community_boundaries LIMIT 10"
+	rows, err := db.Query(testStatement1)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var TripId string
+		err = rows.Scan(&TripId)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(TripId)
+	}
+}
+
+func main() {
+
+	GetAPIrequest(url)
+	// fmt.Printf("%v", Boundaries)
+
+	// Putting this here to eliminate making API calls over and over while testing
+	// SaveTripsJSON("reports.json")
+
+	// Loading from json file to avoid unnecessary API calls
+	// LoadTripsJSON("reports.json")
+
+	// reducing file size to manage Google Cloud credit consumption
+	// LessReports := Reports[0:1000]
+
+	// Drop and re-create table
+	refresh_db_table()
+
+	// Ingest records to DB
+	load_to_db(Boundaries)
+
+	// Query DB to confirm
+	test_successful_insert()
+
+}
